3b2ced2e25a1ba67449709b4bec70f20887400d3
Devo2360 <david.dodoge@gmail.com>
2023-08-20 14:17:23 -0700

    print f devo

--- _printf.c
+++ _printf.c
@@ -8,49 +8,84 @@ void print_buffer(char buffer[], int *buff_ind);
 int _printf(const char *format, ...)
 
 {
-	int i, f = 0, c = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list lists;
-	char buffer[BUFF_SIZE];
 
-	if (format == NULL)
-		return (-1);
+int i, printed = 0, printed_chars = 0;
 
-	va_start(lists, format);
+int flags, width, precision, size, buff_ind = 0;
 
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
+va_list list;
 
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-				/* write(1, &format[i], 1);*/
-			c++;
-		}
+char buffer[BUFF_SIZE];
 
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, lists);
-			precision = get_precision(format, &i, lists);
-			size = get_size(format, &i);
-			++i;
-			f = handle_print(format, &i, lists, buffer,
-			flags, width, precision, size);
-			if (f == -1)
-				return (-1);
+if (format == NULL)
 
-			c += f;
-			}
-		}
+return (-1);
 
-		print_buffer(buffer, &buff_ind);
-		va_end(lists);
-		return (c);
-	}
+va_start(list, format);
+
+for (i = 0; format && format[i] != '\0'; i++)
+
+{
+
+if (format[i] != '%')
+
+{
+
+buffer[buff_ind++] = format[i];
+
+if (buff_ind == BUFF_SIZE)
+
+print_buffer(buffer, &buff_ind);
+
+/* write(1, &format[i], 1);*/
+
+printed_chars++;
+
+}
+
+else
+
+{
+
+print_buffer(buffer, &buff_ind);
+
+flags = get_flags(format, &i);
+
+width = get_width(format, &i, list);
+
+precision = get_precision(format, &i, list);
+
+size = get_size(format, &i);
+
+++i;
+
+printed = handle_print(format, &i, list, buffer,
+
+flags, width, precision, size);
+
+if (printed == -1)
+
+return (-1);
+
+printed_chars += printed;
+
+}
+
+}
+
+
+
+print_buffer(buffer, &buff_ind);
+
+
+
+va_end(list);
+
+
+
+return (printed_chars);
+
+}
 
 
 
@@ -63,8 +98,13 @@ int _printf(const char *format, ...)
 void print_buffer(char buffer[], int *buff_ind)
 
 {
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
 
-	*buff_ind = 0;
+if (*buff_ind > 0)
+
+write(1, &buffer[0], *buff_ind);
+
+
+
+*buff_ind = 0;
+
 }
